72b117ccdab96bbdb5efdd664e455631c86e24ac
Hamza Mohammad <mohd2010hamza@gmail.com>
2023-03-28 09:54:59 +0530

    Made the bars and going to make the next bar

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import theme from "./app/config/colors";
+import Constants from "expo-constants";
+
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			{/* Hours Bar (Contains extra styles) */}
+			<TouchableOpacity
+				style={[styles.longBar, { marginTop: 20 }]}
+				onPress={() => console.log("Hour Pressed")}
+			>
+				<Text style={styles.text}>Hours</Text>
+				<FontAwesome
+					name="angle-right"
+					size={24}
+					color="white"
+					style={styles.icon}
+				/>
+			</TouchableOpacity>
+			{/* Minutes Bar */}
+			<TouchableOpacity
+				style={styles.longBar}
+				onPress={() => console.log("Hour Pressed")}
+			>
+				<Text style={styles.text}>Minutes</Text>
+				<FontAwesome
+					name="angle-right"
+					size={24}
+					color="white"
+					style={styles.icon}
+				/>
+			</TouchableOpacity>
+			{/* AM/PM and findButton Bar */}
+			<View style={styles.buttonContainer}>
+				<View style={styles.amPmButton}></View>
+				<View style={styles.findButton}></View>
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		paddingTop: Constants.statusBarHeight,
+		flex: 1,
+		backgroundColor: "#000",
+	},
+	longBar: {
+		marginHorizontal: 30,
+		paddingRight: 25,
+		height: 62,
+		borderColor: theme.colors.primary,
+		borderWidth: 1.5,
+		borderRadius: 20,
+		marginBottom: 30,
+		alignItems: "center",
+		flexDirection: "row",
+	},
+	text: {
+		color: theme.colors.white,
+		fontSize: theme.fontSize,
+		marginLeft: 20,
+	},
+	icon: {
+		marginLeft: "auto",
+	},
 });
---
+++ app/config/colors.js
@@ -0,0 +1,12 @@
+const theme = {
+	colors: {
+		primary: "#5786FF",
+		primary2: "#E20000",
+		black: "#000",
+		white: "#fff",
+	},
+	fontSize: 28,
+	borderRadius: {},
+};
+
+export default theme;
